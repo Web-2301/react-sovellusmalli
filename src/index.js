47f76fd707a15fa597e89c3e047acd2a72f7b408
60734740 <jukka.aula@kolumbus.fi>
2022-05-13 11:44:08 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/reactodo">
       <App />
     </Router>  
   </React.StrictMode>,
