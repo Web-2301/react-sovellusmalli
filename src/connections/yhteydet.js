a5eb163bc334507b570f8f108a2420a9b59749c5
Web-projektitII <jukka.aula@kolumbus.fi>
2023-05-23 12:08:21 +0300

    porttivertailu

--- src/connections/yhteydet.js
+++ src/connections/yhteydet.js
@@ -2,17 +2,13 @@ let { origin,host,hostname,port,protocol,href } = window.location
 console.log(`window.location:${origin},${host},${hostname},${port},${protocol},${href}`)
 let url = origin
 /* Paikallinen react- ja flask-kehityspalvelin */
-//if (port == '3000') url = url.replace(port,"5000")
-if (false) {}
+if (port != '' && port == '3000') url = url.replace(port,"5000")
+//if (false) {}
 /* XAMPP ja flask-kehityspalvelin */
 else if (host == 'localhost') {
     url = url + ':5000'
     console.log("host:"+host+",url:"+url)
     }
-else if (hostname == 'localhost') {
-    url = url + ':5000'
-    console.log("hostname:"+hostname+",url:"+url)
-    }  
 const baseUrl = url + '/reactapi'
 //const closeUrl = 'http://localhost:5000/reactapi/logout'
 const closeUrl = baseUrl + '/logout'
