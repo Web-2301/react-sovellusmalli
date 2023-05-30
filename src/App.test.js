6a790ab1b64e82addf275098f776607d21faee91
Web-projektitII <jukka.aula@kolumbus.fi>
2023-05-30 20:55:28 +0300

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('root');
+    if (container) {
+      const root = createRoot(container);
+      root.render(<App />);
+      const linkElement = screen.getByText(/learn react/i);
+      expect(linkElement).toBeInTheDocument();
+      }
+  })
+  })
+
